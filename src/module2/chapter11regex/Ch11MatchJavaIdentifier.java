cbff5771603b5841c68a791fbdba72e83942724c
Myat Phone Kyaw <myatphonekyaw5505@gmail.com>
2023-08-12 13:35:24 +0630

    chapter 10

---
+++ src/module2/chapter11regex/Ch11MatchJavaIdentifier.java
@@ -0,0 +1,29 @@
+package module2.chapter11regex;
+
+import java.util.*;
+
+class Ch11MatchJavaIdentifier {
+    /*private static final String STOP = "STOP";
+    private static final String VALID = "Valid Java Identifier";
+    private static final String INVALID = "Not a valid Java identifier";*/
+    private static final String PATTERN = "[a-zA-Z0-9]";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str, reply;
+        while (true) {
+            System.out.print("Identifier: ");str =scanner.next();
+            if(str.equals("STOP"))break;
+            if(str.matches(PATTERN))
+
+            {
+                reply = "Valid Java Identifier";
+            } else
+
+            {
+                reply = "Not a valid Java identifier";
+            }
+            System.out.println(str +": "+reply +"\n");
+        }
+    }
+}
