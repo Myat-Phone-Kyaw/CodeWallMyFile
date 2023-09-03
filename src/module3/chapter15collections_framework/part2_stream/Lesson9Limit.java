2864bf266ae9722a7f9882556014514378e4f650
Myat Phone Kyaw <myatphonekyaw5505@gmail.com>
2023-09-03 13:48:12 +0630

    Stream: Limit

--- src/module3/chapter15collections_framework/part2_stream/Lesson9Limit.java
+++ src/module3/chapter15collections_framework/part2_stream/Lesson9Limit.java
@@ -3,11 +3,11 @@ package module3.chapter15collections_framework.part2_stream;
 import java.util.Arrays;
 import java.util.List;
 
-public class Lesson8Count {
+public class Lesson9Limit {
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(1, 2, 1, 2, 3, 4, 3, 4, 5, 5, 6, 6, 7, 8, 7);
-        System.out.println(integerList.stream().filter(integer -> integer % 2 == 0).count());
+        integerList.stream().distinct().limit(3).forEach(System.out::println);
 
-        System.out.println(integerList.size());
+        integerList.stream().filter(integer -> integer%2==0).distinct().limit(3).forEach(System.out::println);
     }
 }
