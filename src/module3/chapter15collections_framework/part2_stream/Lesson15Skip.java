2476741cb1eee98ffa4ae0caed4226ecc9ae8802
Myat Phone Kyaw <myatphonekyaw5505@gmail.com>
2023-09-03 14:25:09 +0630

    Stream: Skip

---
+++ src/module3/chapter15collections_framework/part2_stream/Lesson15Skip.java
@@ -0,0 +1,12 @@
+package module3.chapter15collections_framework.part2_stream;
+
+import java.util.stream.Stream;
+
+public class Lesson15Skip {
+    public static void main(String[] args) {
+        Stream.of(2,7,1,2,3,8,3,6,4,5,6,7,8)
+                .filter(integer -> integer % 2 ==0)
+                .skip(3)
+                .forEach(System.out::println);
+    }
+}
