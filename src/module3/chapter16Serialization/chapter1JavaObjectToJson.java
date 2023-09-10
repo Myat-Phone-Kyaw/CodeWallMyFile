192367d4713c14627c3018a3675d8c5800e65732
Myat Phone Kyaw <myatphonekyaw5505@gmail.com>
2023-09-10 14:21:59 +0630

    Gson&MysqlConnector Lib & JavaObjectToJson

---
+++ src/module3/chapter16Serialization/chapter1JavaObjectToJson.java
@@ -0,0 +1,32 @@
+package module3.chapter16Serialization;
+
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+public class chapter1JavaObjectToJson {
+    public static void main(String[] args) {
+        Person obj = new Person("Myat Phone Kyaw", 23);
+        Person obj1 = new Person("Maverick", 20);
+        Person obj2 = new Person("Shila", 23);
+        Person obj3 = new Person("Linda", 23);
+
+        List<Person> list = Arrays.asList(obj, obj1, obj2, obj3);
+
+        Gson gson = new Gson();
+        String str = gson.toJson(list);
+        System.out.println(str);
+
+    }
+}
